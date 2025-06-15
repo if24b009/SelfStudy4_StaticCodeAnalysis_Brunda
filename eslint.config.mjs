2d8faf2f71d8bb7ff6738ca3326f54d800e04b5c
Lisa Brunda <if24b009@technikum-wien.at>
2025-06-15 16:30:16 +0200

    Fixed 6 Errors -> adapted eslint.config.mjs to fix undefined module, require, exports, define

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,9 +10,14 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: {
       globals: {
         ...globals.browser,
+        ...globals.node,
         L: "readonly", //fix for 'L' not defined (5 Errors)
+        define: "readonly", //fix for 'define' not defined (2 Error)
       },
-    }
+      parserOptions: { //fix for module, exports, require (4 Errors)
+        sourceType: "module",
+      },
+    },
   },
   tseslint.configs.recommended,
 ]);
